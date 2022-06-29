76de53ec51f62d72bc103de5345ffb3c81320d78
Alexander Gochnour <alexandergochnour@gmail.com>
2022-06-29 17:18:35 -0500

    updated for week 4

---
+++ Week4/4.1.1-SelectingDOMElements.js
@@ -0,0 +1,14 @@
+// select all unordered list (ul) elements
+document.getElementsByTagName("ul");
+document.querySelectorAll (".ul");
+
+// select all elements with the class "class-week"
+documents.getElementsByClassName("class-week");
+document.querySelectorAll(".class-week");
+// select all elements with the class "nav-bar"
+document.getElementsByClassName("nav-bar");
+document.querySelectorAll(".nav-bar");
+// select the element with the id "dog-picture" and save it to a variable
+let dogVar = documents.getElementByID("dog-picture");
+document.querySelectorAll("#dog-picture")
+console.log(unorderedList);
